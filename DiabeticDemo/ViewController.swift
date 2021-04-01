93194a1ef18f5c4438f0e3affb409636b11d8b9d
Jainesh Patel <jhp90189@gmail.com>
2021-04-02 05:14:52 +0530

    Initial Commit

---
+++ DiabeticDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiabeticDemo
+//
+//  Created by Apple on 02/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
