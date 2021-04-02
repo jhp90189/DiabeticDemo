b1ef12bd51b68f1d091d9a2aee3ebf6775ae84d9
Jainesh Patel <jhp90189@gmail.com>
2021-04-03 00:31:49 +0530

    first commit

---
+++ DiabeticDemo/TabularHeaderview.swift
@@ -0,0 +1,20 @@
+//
+//  TabularHeaderview.swift
+//  DiabeticDemo
+//
+//  Created by Apple on 02/04/21.
+//
+
+import UIKit
+
+class TabularHeaderview: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DiabeticDemo/TabularViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TabularViewCell.swift
+//  DiabeticDemo
+//
+//  Created by Apple on 02/04/21.
+//
+
+import UIKit
+
+class TabularViewCell: UICollectionViewCell {
+    
+}
